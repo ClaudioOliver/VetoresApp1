9c898dd5365c3f0db4cc1a8a11ae471135d8319d
Claudio Oliveira <claudio.oliveira127@outlook.com>
2023-02-14 09:15:50 -0300

    Add project files.

---
+++ VetoresApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+namespace VetoresApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           int n = int.Parse(Console.ReadLine());
+
+            double[] vect = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+        }
+    }
+}
